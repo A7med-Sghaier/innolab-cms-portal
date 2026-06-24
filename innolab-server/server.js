e16ef07bec7cfc3c5b1698879ddc05381f642e54
Ahmed Sghaier <a7mado008@gmail.com>
2026-06-24 20:44:04 +0200

    Support node-prefixed builtins on Node 10

--- innolab-server/server.js
+++ innolab-server/server.js
@@ -10,6 +10,20 @@
 
 process.chdir(__dirname);
 
+// Some transitive dependencies now import built-in modules with the `node:`
+// prefix. Node 10 does not support that prefix, so normalize it before Strapi
+// loads the dependency tree.
+const Module = require('module');
+const originalLoad = Module._load;
+
+Module._load = function loadWithNodePrefixCompat(request, parent, isMain) {
+  if (typeof request === 'string' && request.indexOf('node:') === 0) {
+    return originalLoad.call(this, request.slice(5), parent, isMain);
+  }
+
+  return originalLoad.call(this, request, parent, isMain);
+};
+
 (() => {
   const strapi = require('strapi');
   strapi.start();
